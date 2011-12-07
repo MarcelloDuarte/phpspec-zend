96814aa5c34519ea3e6e692fe2aa5dc2fb8a9fae
Marcello Duarte <marcello.duarte@gmail.com>
2011-12-07 19:33:16 +0000

    Added specs for Singularize filter

---
+++ spec/Context/Zend/Filter/SingularizeSpec.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once 'PHPSpec/Context/Zend/Filter/Singularize.php';
+
+use \PHPSpec_Context_Zend_Filter_Singularize as Singularize;
+
+class DescribeSingularize extends \PHPSpec\Context
+{
+    protected $singularize;
+    
+    function before()
+    {
+        $this->singularize = $this->spec(new Singularize);
+    }
+    
+    function itConvertsSimpleCasesToPlural()
+    {
+        $word = 'Authors';
+        $this->singularize->filter($word)->should->be('Author');
+    }
+    
+    function itConvertsMoreComplexCases()
+    {
+        $words = array(
+            'quiz'    => 'quizzes',
+            'ox'      => 'oxen',
+            'mouse'   => 'mice',
+            'house'   => 'houses',
+            'virus'   => 'viruses',
+            'fox'     => 'foxes',
+            'tomato'  => 'tomatoes',
+            'buffalo' => 'buffaloes',
+            'bus'     => 'buses',
+            'octopus' => 'octopuses',
+            'status'  => 'statuses'
+        );
+        
+        foreach ($words as $singular => $word) {
+            $this->singularize->filter($word)->should->be($singular);
+        }
+    }
+    
+    function itConvertsIrregularCases()
+    {
+        $irregular = array(
+            'person' => 'people',
+            'man'    => 'men',
+            'child'  => 'children',
+            'sex'    => 'sexes',
+            'move'   => 'moves'
+        );
+        
+        foreach ($irregular as $singular => $word) {
+            $this->singularize->filter($word)->should->be($singular);
+        }
+    }
+    
+    function itConvertsUncountableCases()
+    {
+        $uncountable = array(
+            'equipment', 'information', 'rice', 'money', 'species', 'series',
+            'fish', 'sheep'
+        );
+        
+        foreach ($uncountable as $word) {
+            $this->singularize->filter($word)->should->be($word);
+        }
+    }
+}
