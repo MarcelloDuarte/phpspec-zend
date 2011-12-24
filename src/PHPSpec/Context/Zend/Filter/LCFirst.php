9b577d72517dc961db590e0ac38b7c4b0b8001bc
Marcello Duarte <marcello.duarte@gmail.com>
2011-12-24 15:31:00 +0000

    Added license header to uc and lc first filters

--- src/PHPSpec/Context/Zend/Filter/LCFirst.php
+++ src/PHPSpec/Context/Zend/Filter/LCFirst.php
@@ -1,10 +1,38 @@
 <?php
+/**
+ * PHPSpec
+ *
+ * LICENSE
+ *
+ * This file is subject to the GNU Lesser General Public License Version 3
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@phpspec.net so we can send you a copy immediately.
+ *
+ * @category  PHPSpec
+ * @package   PHPSpec
+ * @copyright Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                    Marcello Duarte
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ */
 
 /**
  * @see Zend_Filter_Interface
  */
 require_once 'Zend/Filter/Interface.php';
 
+/**
+ * @category   PHPSpec
+ * @package    PHPSpec_Zend
+ * @copyright  Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                     Marcello Duarte
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ */
 class PHPSpec_Context_Zend_Filter_LCFirst implements Zend_Filter_Interface
 {
      
@@ -19,11 +47,18 @@ class PHPSpec_Context_Zend_Filter_LCFirst implements Zend_Filter_Interface
     public function filter($value)
     {
         if (!empty($value)) {
-            $value[0] = strtolower($value[0]) ;
+            $value[0] = strtolower($value[0]);
         }
         return $value;
     }
     
+    /**
+     * Provides a static interface for functional use
+     *
+     * @param string $value
+     * 
+     * @return string String filtered  
+     */
     public static function apply($value)
     {
         $filter = new static;
--- src/PHPSpec/Context/Zend/Filter/UCFirst.php
+++ src/PHPSpec/Context/Zend/Filter/UCFirst.php
@@ -1,5 +1,25 @@
 <?php
-
+/**
+ * PHPSpec
+ *
+ * LICENSE
+ *
+ * This file is subject to the GNU Lesser General Public License Version 3
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@phpspec.net so we can send you a copy immediately.
+ *
+ * @category  PHPSpec
+ * @package   PHPSpec
+ * @copyright Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                    Marcello Duarte
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ */
+ 
 /**
  * @see Zend_Filter_Interface
  */
@@ -19,7 +39,7 @@ class PHPSpec_Context_Zend_Filter_UCFirst implements Zend_Filter_Interface
     public function filter($value)
     {
         if (!empty($value)) {
-            $value[0] = strtoupper($value[0]) ;
+            $value[0] = strtoupper($value[0]);
         }
         return $value;
     }
