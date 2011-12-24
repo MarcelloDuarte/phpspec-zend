e441bc7666e85e42f5ca886deb2051b19ea88ff0
Marcello Duarte <marcello.duarte@gmail.com>
2011-12-24 15:24:09 +0000

    Added license header to zend tool provider classes

--- src/PHPSpec/Context/Zend/Tool/Provider/ViewSpec.php
+++ src/PHPSpec/Context/Zend/Tool/Provider/ViewSpec.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * PHPSpec
+ *
+ * LICENSE
+ *
+ * This file is subject to the GNU Lesser General Public License Version 3
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@phpspec.net so we can send you a copy immediately.
+ *
+ * @category  PHPSpec
+ * @package   PHPSpec
+ * @copyright Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                    Marcello Duarte
+ * @license   http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ */
+
 require_once 'PHPSpec/Context/Zend/Filter/LCFirst.php';
 require_once 'PHPSpec/Context/Zend/Filter/UCFirst.php';
 
@@ -7,6 +28,14 @@ use PHPSpec_Context_Zend_Filter_LCFirst as LCFirst,
     PHPSpec_Context_Zend_Filter_UCFirst as UCFirst,
     Zend_Filter_Word_DashToCamelCase as DashToCamelCase;
 
+/**
+ * @category   PHPSpec
+ * @package    PHPSpec_Zend
+ * @copyright  Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
+ * @copyright  Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ *                                     Marcello Duarte
+ * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
+ */
 class PHPSpec_Context_Zend_Tool_Provider_ViewSpec
     extends Zend_Tool_Project_Provider_View
     implements Zend_Tool_Framework_Provider_Pretendable
