5753cb97b11948cb85bac36d87968ba582351e5e
Marcello Duarte <marcello.duarte@gmail.com>
2011-12-29 12:38:53 +0000

    Changed copyright date

--- src/PHPSpec/Context/Zend/Tool/Context/ModelFile.php
+++ src/PHPSpec/Context/Zend/Tool/Context/ModelFile.php
@@ -15,7 +15,7 @@
  * @category  PHPSpec
  * @package   PHPSpec
  * @copyright Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
- * @copyright Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ * @copyright Copyright (c) 2010-2012 Pádraic Brady, Travis Swicegood,
  *                                    Marcello Duarte
  * @license   http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
  */
@@ -31,7 +31,7 @@ use PHPSpec_Context_Zend_Filter_UCFirst as UCFirst,
  * @category   PHPSpec
  * @package    PHPSpec_Zend
  * @copyright  Copyright (c) 2007-2009 Pádraic Brady, Travis Swicegood
- * @copyright  Copyright (c) 2010-2011 Pádraic Brady, Travis Swicegood,
+ * @copyright  Copyright (c) 2010-2012 Pádraic Brady, Travis Swicegood,
  *                                     Marcello Duarte
  * @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU Lesser General Public Licence Version 3
  */
